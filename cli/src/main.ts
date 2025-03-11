ef548d375e7cb4331a21372e1a661dc7ee9857fc
summerlinryan <summerlinryan@gmail.com>
2025-03-11 15:55:58 -0400

    Add CLI project back to repo

---
+++ cli/src/main.ts
@@ -0,0 +1,17 @@
+import { Command } from "commander";
+import scan from "./scan.js";
+
+const program = new Command();
+
+program
+  .command("scan <folder>")
+  .description(
+    "Scan the specified repository folder for TODO/FIXME comments and create scheduled notifications",
+  )
+  .action(async (dir: string) => {
+    const todos = await scan(dir);
+    console.log(todos);
+  });
+
+program.parse(process.argv);
+// TODO: add a command to send notifications
